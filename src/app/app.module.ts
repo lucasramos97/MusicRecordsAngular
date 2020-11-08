88bfcb042419143641bc9a9ac0b5c4bf93736aa9
lucasramos97 <lucasramosdev@gmail.com>
2020-11-08 14:47:07 -0300

    Replacement of the Bootstrap styling library for that of the PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -13,6 +14,7 @@ import { MusicsModule } from './musics/musics.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     MusicsModule
